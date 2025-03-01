4b50f60570075ed0580abdf500e38cb8f224b1fb
learn <yubhawj86@gmail.com>
2025-03-01 17:10:38 +0700

    kk

--- src/routers/test.js
+++ src/routers/test.js
@@ -1,1 +1,22 @@
-const branch1 = "Branch Test";
+const change_branch_master = "for test add";
+const test3 = "for test add";
+const test4 = "for test add";
+
+const test5 = "for test add";
+const test9 = "for test add";
+
+
+import { create, list, listBy, update, remove } from "../controllers/cars.js";
+import { Router } from "express";
+
+const router = Router();
+const car = "/car";
+
+router.post(car, create);
+router.get(car, listasfads);
+router.get(car + "/:id", listBasfdsfadsy);
+router.put(car + "/:id", update);
+router.delete(car + "/:id", remove);
+
+export default router;
+
