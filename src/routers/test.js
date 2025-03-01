45fb30949c45fa27a4b2f4cfcb7d0c83801e43b6
learn <yubhawj86@gmail.com>
2025-03-01 16:05:16 +0700

    kk

--- src/routers/test.js
+++ src/routers/test.js
@@ -8,6 +8,19 @@ const test5 = "for test add";
 const test9 = "for test add";
 
 
+import { create, list, listBy, update, remove } from "../controllers/cars.js";
+import { Router } from "express";
+
+const router = Router();
+const car = "/car";
+
+router.post(car, create);
+router.get(car, list);
+router.get(car + "/:id", listBy);
+router.put(car + "/:id", update);
+router.delete(car + "/:id", remove);
+
+export default router;
 
 const new_test = "for test add";
 const new_testadd = "for test add";
