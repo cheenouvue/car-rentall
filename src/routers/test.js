b72b03d532325beb4ceb12e4e1121b5df98128e9
learn <yubhawj86@gmail.com>
2025-03-01 16:02:27 +0700

    kk

--- src/routers/test.js
+++ src/routers/test.js
@@ -3,7 +3,11 @@ const test1 = "for test add";
 const test2 = "for test add";
 const test3 = "for test add";
 const test4 = "for test add";
+
 const test5 = "for test add";
 const test9 = "for test add";
-const change = "for test add";
+
+const change_new = "for test add";
+
 const new_test = "for test add";
+const new_testadd = "for test add";
