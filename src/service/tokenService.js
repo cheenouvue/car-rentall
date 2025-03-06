3d5b08a5fc46774cd29435ba7cf44f8c650820f6
learn <yubhawj86@gmail.com>
2025-03-06 10:42:36 +0700

    start github pull

--- src/service/tokenService.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const generateAccessToken = (user) => {
-    return jwt.sign({ userId: user.id, userRole: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-};
-
-export const generateRefreshToken = (userId) => {
-    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
-};
